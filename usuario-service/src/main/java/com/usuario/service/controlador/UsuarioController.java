83ae0b17cfb9c317560940cc605cfa1b5bf494ca
MagDev <iscmagc24@gmail.com>
2022-05-11 18:29:53 -0500

    RestTemplate - FeignClient

---
+++ usuario-service/src/main/java/com/usuario/service/controlador/UsuarioController.java
@@ -0,0 +1,95 @@
+package com.usuario.service.controlador;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usuario.service.entidades.Usuario;
+import com.usuario.service.modelos.Carro;
+import com.usuario.service.modelos.Moto;
+import com.usuario.service.servicio.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	//Listar usuarios
+	@GetMapping
+	public ResponseEntity<List<Usuario>> listarUsuarios(){
+		List<Usuario> usuarios = usuarioService.getAll();
+		if (usuarios.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(usuarios);
+	}
+	
+	//Buscar un usuario por ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> obtenerUsuario(@PathVariable("id") int id){
+		Usuario usuario = usuarioService.getUsuarioById(id);
+		if (usuario == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(usuario);
+	}
+	
+	//Insertar usuario
+	@PostMapping
+	public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario){
+		Usuario nuevoUsuario = usuarioService.save(usuario);
+		return ResponseEntity.ok(nuevoUsuario);
+	}
+	
+	//Buscar todos los carros que pertenecen a un usuario
+	@GetMapping("/carros/{usuarioId}")
+	public ResponseEntity<List<Carro>> listarCarros(@PathVariable("usuarioId") int usuarioId){
+		Usuario usuario = usuarioService.getUsuarioById(usuarioId);
+		if (usuario == null) {
+			return ResponseEntity.notFound().build();
+		}
+		List<Carro> carros = usuarioService.getCarros(usuarioId);
+		return ResponseEntity.ok(carros);
+	}
+	
+	//Buscar todos las motos que pertenecen a un usuario
+	@GetMapping("/motos/{usuarioId}")
+	public ResponseEntity<List<Moto>> listarMotos(@PathVariable("usuarioId") int usuarioId){
+		Usuario usuario = usuarioService.getUsuarioById(usuarioId);
+		if (usuario == null) {
+			return ResponseEntity.notFound().build();
+		}
+		List<Moto> motos = usuarioService.getMotos(usuarioId);
+		return ResponseEntity.ok(motos);
+	}
+	
+	//Registrar un carro del usuario
+	@PostMapping("/carro/{usuarioId}")
+	public ResponseEntity<Carro> guardarCarro(@PathVariable("usuarioId") int usuarioId, @RequestBody Carro carro){
+		Carro nuevoCarro = usuarioService.saveCarro(usuarioId, carro);
+		return ResponseEntity.ok(nuevoCarro);
+	}
+	
+	//Registrar una moto del usuario
+	@PostMapping("/moto/{usuarioId}")
+	public ResponseEntity<Moto> guardarMoto(@PathVariable("usuarioId") int usuarioId, @RequestBody Moto moto){
+		Moto nuevaMoto = usuarioService.saveMoto(usuarioId, moto);
+		return ResponseEntity.ok(nuevaMoto);
+	}
+	
+	@GetMapping("/todos/{usuarioId}")
+	public ResponseEntity<Map<String, Object>> listarTodosLosVehiculos(@PathVariable("usuarioId") int usuarioId){
+		Map<String, Object> resultado = usuarioService.getUsuarioAndVehiculos(usuarioId);
+		return ResponseEntity.ok(resultado);
+	}
+}
